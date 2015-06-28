6160ec2d780c297220df3a153b906fe5d2c06e22
Chris Felton <chris.felton@gmail.com>
2015-06-28 15:02:20 -0500

    ci now use the latest myhdl

--- scripts/ci/install_myhdl.sh
+++ scripts/ci/install_myhdl.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 git clone https://github.com/jandecaluwe/myhdl
 cd myhdl
-git checkout 88cb76b
+#git checkout 88cb76b
 python setup.py install
 make -C cosimulation/icarus
 # copy the VPI to the test directory
