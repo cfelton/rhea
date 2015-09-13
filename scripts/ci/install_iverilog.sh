c8f587edfc6722f0744690c0a6caa66c6207f7a7
Christopher Felton <chris.felton@gmail.com>
2015-09-13 00:21:26 -0500

    Update install_iverilog.sh

--- scripts/ci/install_iverilog.sh
+++ scripts/ci/install_iverilog.sh
@@ -7,6 +7,7 @@ if [ ! -d "$HOME/iverilog/driver" ]; then
     sh autoconf.sh
     ./configure --prefix=$HOME/iverilog
     make
+    make check
     make install
 else
   echo 'Using cached directory.';
