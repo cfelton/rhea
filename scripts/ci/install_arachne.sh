b12271f50b14a971373be3ba3a4b4c8dc20034c7
Christopher Felton <chris.felton@gmail.com>
2015-09-23 10:19:29 -0500

    Update install_arachne.sh

--- scripts/ci/install_arachne.sh
+++ scripts/ci/install_arachne.sh
@@ -2,10 +2,9 @@
 set -e
 # check to see if arachne-pnr folder is empty
 if [ ! -d "$HOME/arachne-pnr/bin/" ]; then
-  git clone https://github.com/cseed/arachne-pnr
-  cd arachne-pnr
+  git clone https://github.com/cseed/arachne-pnr $HOME/arachne-pnr
+  cd $HOME/arachne-pnr
   make
-  cd ..
 else
   echo 'Using cached directory.';
 fi
