b1e89b60e62a6f6909bc46b50481895c66a9259c
Chris Felton <chris.felton@gmail.com>
2015-09-22 22:43:06 -0500

    checkpoint: icestick example start

--- examples/boards/icestick/icestick.py
+++ examples/boards/icestick/icestick.py
@@ -1,11 +1,36 @@
 
+from __future__ import print_function
+from __future__ import division
 
-from myhdl import Signal, intbv
+from myhdl import Signal, intbv, instances
+
+from rhea.cores.uart import uart_lite
+from rhea.cores.misc import glbl_timer_ticks
+from rhea.system impor Global
+from rhea.system import FIFOBus
 
 
-def icestick(clock, reset, led, pmodled, uart):
+def icestick(
+    # ports
+    clock, reset, led, pmod, 
+    uart_tx, uart_rx
+    # parameters
+):
     """
     """
+    
+    glbl = Global(clock, reset)    
+    gticks = glbl_timer_ticks(glbl, include_seconds=True)
 
+    # get interfaces to the UART fifos
+    fbustx, fbusrx = (FIFOBus(width=8, size=8), 
+                      FIFOBus(width=8, size=8), )
+
+    # get the UART comm from PC
+    guart = uart_lite(glbl, fbustx, fbusrx, uart_tx, uart_rx)
+
+    # system to test/interface
     
-    
+    # other stuff
+
+    return instances()
