a4cca706206db050d86ac0b9658d08d5bd3553b2
Christopher Felton <chris.felton@gmail.com>
2015-09-23 00:25:51 +0000

    add simple test for video frame image generation

---
+++ test/test_cores/test_video/test_create_image.py
@@ -0,0 +1,30 @@
+
+import os 
+from random import randint
+from rhea.models.video import VideoDisplay
+
+
+def test_create_save():
+    num_frames = 3
+    #resolution = res = (randint(32, 800), randint(32, 800),)
+    resolution = res =  (32, 15,)
+    color_depth = cd = (8, 8, 8)
+    disp = VideoDisplay(resolution=resolution,
+                        color_depth=color_depth)
+    print(str(disp))
+    for ii in range(num_frames):      
+        for row in range(res[1]):
+            for col in range(res[0]):
+                rgb = (randint(0, cd[0]-1), 
+                       randint(0, cd[1]-1), 
+                       randint(0, cd[2]-1))
+                last = (row == res[1]-1 and col == res[0]-1)
+                if last:
+                    print("End of frame {}, {}, {}".format(row, col, rgb))
+                #print(col, row, rgb, last)
+                disp.set_pixel(col, row, rgb, last)
+                
+                
+if __name__ == '__main__':
+    test_create_save()
+            
