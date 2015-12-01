68185e68f78d42c2bd6c9cdc069f89df6320ee98
Chris Felton <chris.felton@gmail.com>
2015-12-01 07:29:55 -0600

    fixed missing renames

--- rhea/build/toolflow/xilinx/ise.py
+++ rhea/build/toolflow/xilinx/ise.py
@@ -34,7 +34,7 @@ class ISE(ToolFlow):
         Given a top-level module (function) and a board definition
         create an instance of the ISE tool-chain.
         """
-        _toolflow.__init__(self, brd, top=top, path=path)
+        super(ISE, self).__init__(brd=brd, top=top, path=path)
         #self.reports = _ise_parse_reports(self)
         self.ucf_file = ''
 
