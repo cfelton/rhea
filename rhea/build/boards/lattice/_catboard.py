06a3a8966f40a6b3c31aca6c6fee37719f0312c3
Chris Felton <chris.felton@gmail.com>
2016-02-09 07:03:37 -0600

    added more catboard default port assignments

--- rhea/build/boards/lattice/_catboard.py
+++ rhea/build/boards/lattice/_catboard.py
@@ -21,21 +21,39 @@ class CATBoard(FPGA):
     
     default_ports = {
         # the default port names match thoses in the schematic
-        # but lower-case
+        # but lower-case (and a couple exceptions)
         'led': dict(pins=('A9', 'B8', 'A7', 'B7',)),
         'sw': dict(pins=('A16', 'B9',)),
         'dipsw': dict(pins=('C6', 'C5', 'C4', 'C3',)),
-        'hdr1': dict(pins=('J1', 'K1', 'H1', 'J2', )),
+        # the header index will not match the numbering on the
+        # schematic because the nets in the schematic match
+        # the header physical pins
+        'hdr1': dict(pins=('J1', 'K1', 'H1', 'J2', 'H2', 'G2',
+                           'G1', 'F2', 'F1', 'E2', 'F3', 'D1',
+                           'E3', 'D2', 'C2', 'C1', 'B2', 'B1')),
         'bcm14_txd': dict(pins='T15'),
         'bcm15_rxd': dict(pins='T14'),
-        # @todo: finish default ports
+        
+        'bcm9_miso': dict(pins='P12'),
+        'bcm10_mosi': dict(pins='P11'),
+        'bcm11_sclk': dict(pins='R11'),
+        'bcm25': dict(pins='R12'),
 
+        'bcm19_miso': dict(pins='T3'),
+        'bcm20_mosi': dict(pins='R3'),
+        'bcm21_sclk': dict(pins='T1'),
+        'bcm16': dict(pins='R4'),
+        'bcm26': dict(pins='T2'),
+        'bcm3_scl': dict(pins='T16'),
+        'bcm2_sda': dict(pins='R16'),
+        
+        # @todo: finish default ports
     }
     
     default_extintf = {
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # SDRAM: the Xula2 has a 256Mbit WINBond SDRAM,        
-        # @todo: merge into rhea.syste/rhea.cores interfaces
+        # @todo: merge into rhea.system/rhea.cores interfaces
         'sdram': ExternalInterface(
             Port('addr', pins=('F13', 'E14', 'E13', 'D14',  # 0-3
                                'B16', 'C16', 'D15', 'D16',  # 4-7
