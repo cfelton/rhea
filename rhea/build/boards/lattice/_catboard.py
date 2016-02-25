5c9f0139091df95ea824884ed7c287c5992cf472
Christopher Felton <chris.felton@gmail.com>
2016-02-25 06:31:02 -0600

    formatting changes

--- rhea/build/boards/lattice/_catboard.py
+++ rhea/build/boards/lattice/_catboard.py
@@ -55,38 +55,38 @@ class CATBoard(FPGA):
         # SDRAM: the Xula2 has a 256Mbit WINBond SDRAM,        
         # @todo: merge into rhea.system/rhea.cores interfaces
         'sdram': ExternalInterface(
-            Port('addr', pins=('F13', 'E14', 'E13', 'D14',  # 0-3
-                               'B16', 'C16', 'D15', 'D16',  # 4-7
-                               'E16', 'F15', 'F14', 'F16',  # 8-11
-                               'G14',)                      # 12
-                 ),
-            Port('data', pins=('R14', 'P14', 'M13', 'M14',  # 0-3
-                               'L13', 'L14', 'K13', 'K14',  # 4-7
-                               'J16', 'L16', 'M16', 'M15',  # 8-11
-                               'N16', 'P16', 'P15', 'R15',) # 12-15
-             ),
-            Port('bs',   pins=('H14', 'G13',) ),
-            Port('cas',  pins=('K15',) ),
-            Port('ras',  pins=('K16',) ),
-            Port('ldqm', pins=('J13',) ),
-            Port('udqm', pins=('J15',) ),
-            Port('clk',  pins=('H16',) ),
-            Port('clkfb', pins=('G16',) ),
-            Port('cs', pins=('H13',) ),
-            Port('we', pins=('J14',) ),
+            Port('addr', pins=('F13', 'E14', 'E13', 'D14',   # 0-3
+                               'B16', 'C16', 'D15', 'D16',   # 4-7
+                               'E16', 'F15', 'F14', 'F16',   # 8-11
+                               'G14',)                       # 12
+            ),
+            Port('data', pins=('R14', 'P14', 'M13', 'M14',   # 0-3
+                               'L13', 'L14', 'K13', 'K14',   # 4-7
+                               'J16', 'L16', 'M16', 'M15',   # 8-11
+                               'N16', 'P16', 'P15', 'R15',)  # 12-15
+            ),
+            Port('bs',   pins=('H14', 'G13',)),
+            Port('cas',  pins=('K15',)),
+            Port('ras',  pins=('K16',)),
+            Port('ldqm', pins=('J13',)),
+            Port('udqm', pins=('J15',)),
+            Port('clk',  pins=('H16',)),
+            Port('clkfb', pins=('G16',)),
+            Port('cs', pins=('H13',)),
+            Port('we', pins=('J14',)),
             Port('cke',  pins=('G15',)),
             
             # timing information, all in ns
             timing = dict(
-                init = 200000.0,
-                ras  = 45.0,
-                rcd  = 20.0,
-                ref  = 64000000.0,
-                rfc  = 65.0,
-                rp   = 20.0,
-                xsr  = 75.0                  
+                init=200000.0,
+                ras=45.0,
+                rcd=20.0,
+                ref=64000000.0,
+                rfc=65.0,
+                rp=20.0,
+                xsr=75.0
              ),
-            ddr = 0  # single data rate
+            ddr=0  # single data rate
         ),
         
     }
