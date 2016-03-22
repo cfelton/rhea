02db040756f40373db7f484e41e35d46c568ac89
forumulator <forumulator@gmail.com>
2016-03-22 18:31:46 +0530

    Deleted duplicate conftest.py

--- examples/boards/conftest.py
+++
@@ -1,8 +0,0 @@
-# content of conftest.py
-import pytest
-
-def pytest_addoption(parser):
-    parser.addoption("--runslow", action="store_true",
-        help="run slow tests(not video)")
-    parser.addoption("--vidtest", action="store_true",
-        help="run slow video tests")
